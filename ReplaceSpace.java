958af906447fd39d6cf3e793054fa453b974ef4f
181114mly <1752262568@qq.com>
2020-05-03 23:14:50 +0800

    替换空格

---
+++ ReplaceSpace.java
@@ -0,0 +1,17 @@
+package LeetCode;
+//替换空格
+public class ReplaceSpace {
+    public String replaceSpace(StringBuffer str) {
+     if(str==null){
+         return null;
+     }
+     for(int i=0;i<str.length();i++){
+         char c=str.charAt(i);
+         if(c==' '){
+            str.replace(i,i+1,"%20");
+         }
+     }
+     String s=str.toString();
+     return s;
+    }
+}
