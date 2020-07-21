801487756529d57e4e38424db92185c87d7c6006
181114mly <58422408+181114mly@users.noreply.github.com>
2020-07-22 07:35:18 +0800

    Update Replacement.java

--- Replacement.java
+++ Replacement.java
@@ -10,6 +10,7 @@ public class Replacement {
         return iniString;
     }
 }
+//方法二 StringBuffer的append()方法
 class Replacement2{
     public String replaceSpace(String iniString, int length){
         StringBuffer sb=new StringBuffer();
@@ -22,5 +23,32 @@ class Replacement2{
             }
         }
         return sb.toString();
+    }
+}
+//方法三 字符串移动
+class Replement3 {
+    public String replaceSpace(StringBuffer str) {
+    	int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                count++;
+            }
+        } 
+        int newLength=str.length()+2*count;
+        int old_end=str.length()-1;
+        int new_end=newLength-1;
+        str.setLength(newLength);
+        while(old_end>=0&&new_end>=0){
+            if(str.charAt(old_end)==' '){
+               str.setCharAt(new_end--,'0');
+               str.setCharAt(new_end--,'2');
+               str.setCharAt(new_end--,'%');
+                --old_end;
+            }else{            
+                str.setCharAt(new_end--,str.charAt(old_end));
+                 --old_end;
+            }
+        }
+        return str.toString();
     }
 }
